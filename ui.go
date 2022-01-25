1b32aecafe988208e2e7838ea3fcf66296e6e73f
alihan.yalcin <alihan.yalcin@trendyol.com>
2022-01-25 11:42:20 +0300

    add update modal

--- ui.go
+++ ui.go
@@ -1,25 +1,30 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
 type view struct {
-	app       *tview.Application
-	pages     *tview.Pages
-	table     *tview.Table
-	quitModal *tview.Modal
+	app    *tview.Application
+	pages  *tview.Pages
+	table  *tview.Table
+	update *tview.Modal
+	quit   *tview.Modal
 }
 
 func Start() {
 	v := &view{
-		app:       tview.NewApplication(),
-		pages:     tview.NewPages(),
-		table:     tview.NewTable(),
-		quitModal: tview.NewModal(),
+		app:    tview.NewApplication(),
+		pages:  tview.NewPages(),
+		table:  tview.NewTable(),
+		update: tview.NewModal(),
+		quit:   tview.NewModal(),
 	}
 
+	v.setUpdateModal()
 	v.setQuitModal()
 	v.setTable()
 
@@ -28,8 +33,16 @@ func Start() {
 	}
 }
 
+func (v *view) setUpdateModal() {
+	v.update.SetText("Do you want to update?").
+		AddButtons([]string{"Yes", "No"})
+
+	v.pages.AddPage("update", v.update, true, true)
+	v.pages.SendToBack("update")
+}
+
 func (v *view) setQuitModal() {
-	v.quitModal.SetText("Do you want to quit?").
+	v.quit.SetText("Do you want to quit?").
 		AddButtons([]string{"Quit", "Cancel"}).
 		SetDoneFunc(func(buttonIndex int, buttonLabel string) {
 			if buttonLabel == "Quit" {
@@ -39,7 +52,7 @@ func (v *view) setQuitModal() {
 			}
 		})
 
-	v.pages.AddPage("quit", v.quitModal, true, true)
+	v.pages.AddPage("quit", v.quit, true, true)
 	v.pages.SendToBack("quit")
 }
 
@@ -81,8 +94,16 @@ func (v *view) setTable() {
 			}
 		}).
 		SetSelectedFunc(func(row int, column int) {
-			// TODO: add update modal
-			v.table.GetCell(row, column).SetTextColor(tcell.ColorRed)
+			v.pages.SendToFront("update")
+			v.update.SetText(fmt.Sprintf("Do you want to update %s?", dependencies[row-1].path)).
+				SetDoneFunc(func(buttonIndex int, buttonLabel string) {
+					if buttonLabel == "Yes" {
+						// TODO:  update
+						v.table.GetCell(row, column).SetTextColor(tcell.ColorRed)
+					}
+					v.pages.SendToBack("update")
+
+				})
 		})
 
 	v.pages.AddPage("table", v.table, true, true)
